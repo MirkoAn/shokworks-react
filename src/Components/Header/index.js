fae211e7f1e1f6a999185df092ecb2dc7e952ea2
Mirko <mirko2792@gmail.com>
2021-02-23 00:23:21 -0500

    Finished the header-component for mobile devices, including the toggle menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,20 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Logo from '../../assets/images/shokworks-logo.svg'
 
-import { Headers, Nav, List, Search } from './styles'
+import { HeaderWrapper, MobileMenu, Image, MenuToggle, Nav, List, Search, Button } from './styles'
 
 import { MdSearch } from 'react-icons/md'
 
 export const Header = () => {
+  const [open, setOpen] = useState(true)
+  const toggleMenu = () => {
+    setOpen(!open)
+  }
   return (
     <>
-      <Headers className='site-header'>
-        <Nav className='primary-navbar'>
-          <div className='primary-navbar__logo'>
-            <img src={Logo} alt='shokworks-logo' />
+      <HeaderWrapper id='header'>
+        <MobileMenu className='primary-navbar__logo'>
+          <Image src={Logo} alt='shokworks-logo' />
+          <MenuToggle onClick={toggleMenu} open={open}>
+            <svg height='24' viewBox='0 -53 384 384' width='24' xmlns='http://www.w3.org/2000/svg'><path d='m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0' /><path d='m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0' /><path d='m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0' /></svg>
+          </MenuToggle>
+        </MobileMenu>
+        <Nav open={open}>
+          <List open={open}>
+            <li><a href='#'>home</a></li>
+            <li><a href='#'>about</a></li>
+            <li><a href='#'>portfolio</a></li>
+            <li><a href='#'>product</a></li>
+            <li><a href='#'>career</a></li>
+            <li><a href='#'>blog</a></li>
+            <div className='primary-navbar__search-container'>
+              <Search className='search'>
+                <input className='SearchBox' type='search' placeholder='Search Here' name='search' />
+                <MdSearch size='24px' className='SearchIcon' />
+              </Search>
+            </div>
+            <Button>
+              <a href='#' className='btn'>Contact us</a>
+            </Button>
+          </List>
+        </Nav>
+        {/* <div className='site-header__search-container items'>
+          <div className='search'>
+            <input class='searchBox' type='search' placeholder='Search Here' name='search' />
           </div>
-          <List className='primary-navbar__items'>
+        </div>
+        // <div class='site-header__btn-container items'>
+        //   <a href='#' class='btn'>Contact us</a>
+        // </div> */}
+        {/* <List className='primary-navbar__items'>
             <li><a href='#'>home</a></li>
             <li><a href='#'>about</a></li>
             <li><a href='#'>portfolio</a></li>
@@ -22,17 +55,8 @@ export const Header = () => {
             <li><a href='#'>career</a></li>
             <li><a href='#'>blog</a></li>
           </List>
-          <div className='primary-navbar__search-container'>
-            <Search className='search'>
-              <input className='SearchBox' type='search' placeholder='Search Here' name='search' />
-              <MdSearch size='24px' className='SearchIcon' />
-            </Search>
-          </div>
-          <div className='primary-navbar__btn-container'>
-            <a href='#' className='btn'>Contact us</a>
-          </div>
-        </Nav>
-      </Headers>
+           */}
+      </HeaderWrapper>
     </>
   )
 }
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,19 +1,49 @@
 import styled from 'styled-components'
 
-export const Headers = styled.header`
-  padding: 37px 50px;
+import * as fonttheme from '../../styles/variables/FontThemes'
+import * as colors from '../../styles/variables/Colors'
+
+export const HeaderWrapper = styled.header`
+  padding: 20px 10px 0px 20px;
+`
+
+export const MobileMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`
+
+export const Image = styled.img`
+  width: 60%;
+`
+
+export const MenuToggle = styled.div`
+  z-index: 1;
+  cursor: pointer;
 `
 
 export const Nav = styled.nav`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  padding: 0 10px 0 10px;
-  font-family: 'Poppins', sans-serif;
+  font-family: ${fonttheme.font1};
   text-transform: capitalize;
+  position: fixed;
+  display: ${(props) => (props.open ? 'flex' : 'none')};
 `
-export const List = styled.ul`
-  display: flex;
+
+export const List = styled.div`
+
+  li {
+    list-style: none;
+    margin-top: 20px;
+  }
+
+  a {
+    color: ${colors.primarycolor};
+    &:hover {
+      color: ${colors.secondarycolor};
+    }
+  }
+  
+  /* display: flex;
   li {
         flex-grow: 1;
         padding-right: 42px;
@@ -23,21 +53,65 @@ export const List = styled.ul`
   a {
         display: block;
         color: #555555;
-     }
+     } */
 `
 
 export const Search = styled.div`
   display: grid;
   grid-template-columns: 1fr auto;
+  margin-top: 20px;
+
+  input[type="search"]::-webkit-search-cancel-button {
+    display: none;
+  }
+
   input {
+
     grid-column: 1/3;
     grid-row: 1/2;
-    border-radius: 100px;
-    border: 1px solid #828282;
-    border-radius: 100px;    
+    width: 280px;
+    height: 50px;
+    padding: 5px 10px;
+    border-radius: 4px;
+    border: 1px solid #C6BAF8;
+    font-family: ${fonttheme.font1};
+
+    &::placeholder {
+      color: #CCCCCC;
+    }
+
+    &:focus {
+      outline: none;
+    }
   }
   .SearchIcon {
     grid-column: 2/3;
     grid-row: 1/2;
+    margin: 13px 5px;
+    cursor: pointer;
+    color: #C6BAF8;
+  }
+`
+
+export const Button = styled.div`
+  background: ${colors.btncolor};
+  margin-top: 20px;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 4px;
+  text-align: center;
+
+  a {
+    color: ${colors.backgroundcolor};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 30px;
+    width: 169px;
+    height: 58px;
+
+    &:hover {
+      color: ${colors.backgroundcolor};
+    }
   }
 `
