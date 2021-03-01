cc7e5a83f4db5f3524782146927a1716456e5af7
Mirko <mirko2792@gmail.com>
2021-03-01 12:07:16 -0500

    Finished styles for all devices

--- src/Components/Support/styles.js
+++ src/Components/Support/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { mediaQueries } from '../../styles/MediaQueries/MediaQueries'
+
 import * as fonttheme from '../../styles/variables/FontThemes'
 import * as colors from '../../styles/variables/Colors'
 
@@ -11,11 +13,28 @@ export const SupportWrapper = styled.section`
   justify-content: center;
   align-items: center;
   text-align: center;
+  ${mediaQueries('laptop')`
+    flex-direction: row;
+    text-align: left;
+    margin-left: 150px;
+  `}
+
+  ${mediaQueries('laptopxl')`
+  gap: 72px;
+  `}
 `
 export const SupportImg = styled.img`
   width: 70%;
+  ${mediaQueries('laptopxl')`
+      width: 100%;
+    `}
 `
 export const Container = styled.div`
+
+  ${mediaQueries('laptop')`
+      width: 540px;
+      margin-right: 50px;
+    `}
   h2 {
     text-transform: capitalize;
     font-size: 30px;
@@ -23,17 +42,25 @@ export const Container = styled.div`
     margin-bottom: 20px;
     margin-top: 20px;
     font-family: ${fonttheme.font2};
+
+    ${mediaQueries('laptop')`
+      margin-bottom: 40px;
+      font-size: 43px;
+    `}
   }
 
   p {
     font-size: 14px;
     font-family: ${fonttheme.font1};
     color: ${colors.primarycolor};
+    ${mediaQueries('laptop')`
+      font-size: 18px;
+    `}
   }
   span {
     font-weight: 600;
   }
 `
 export const ImgWrapper = styled.div`
-  background-image : url(${BgImg})
+  background-image : url(${BgImg});
 `
