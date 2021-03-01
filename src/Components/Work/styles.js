ef2393764007e4908e99d526fdf919ca9f5f098d
Mirko <mirko2792@gmail.com>
2021-03-01 10:26:19 -0500

    Finished work-component for larger devices

--- src/Components/Work/styles.js
+++ src/Components/Work/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import * as fonttheme from '../../styles/variables/FontThemes'
 import * as colors from '../../styles/variables/Colors'
 
+import { mediaQueries } from '../../styles/MediaQueries/MediaQueries'
+
 import BgImg from '../../assets/images/Group2258.png'
 
 export const Container = styled.section`
@@ -12,6 +14,12 @@ export const Container = styled.section`
   justify-content: center;
   margin-top: 50px;
   text-align: center;
+
+  ${mediaQueries('laptop')`
+    flex-direction: row-reverse;
+    text-align: left;
+    margin-left: 150px;
+  `}
 `
 export const Button = styled.div`
   display: flex;
@@ -24,6 +32,10 @@ export const Button = styled.div`
   height: 58px;
   background: ${colors.backgroundcolor};
   border: 1px solid ${colors.secondarycolor};
+
+  ${mediaQueries('laptop')`
+    margin: 45px 0 0 0;
+  `}
   &:hover {
     background: ${colors.secondarycolor};
     transition: 0.5s;
@@ -43,10 +55,17 @@ export const Button = styled.div`
 `
 export const Wrapper = styled.div`
 
+  ${mediaQueries('laptop')`
+    width: 580px;
+  `}
+
   p {
     font-size: 14px;
     font-family: ${fonttheme.font1};
     color: ${colors.primarycolor};
+    ${mediaQueries('laptop')`
+    font-size: 15px;
+  `}
   }
 
   h2 {
@@ -54,7 +73,13 @@ export const Wrapper = styled.div`
     margin-bottom: 20px;
     margin-top: 20px;
     font-size: 30px;
-    font-family: ${fonttheme.font2}
+    font-weight: 500;
+    font-family: ${fonttheme.font2};
+
+    ${mediaQueries('laptop')`
+    font-size: 43px;
+    margin-bottom: 40px;
+  `}
   }
 
   span {
