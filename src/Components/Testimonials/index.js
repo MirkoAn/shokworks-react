6fbfd27801cf8e9a3da522f8d1daec3125ec58e0
Mirko <mirko2792@gmail.com>
2021-03-01 23:24:08 -0500

    Completed UI for all devices

--- src/Components/Testimonials/index.js
+++ src/Components/Testimonials/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-import { Card, AvatarImg, ArrowContainer } from './styles'
+import { Card, AvatarImg, ArrowContainer, RichardImg, RebecaImg, SadieImg, EricImg } from './styles'
 
 import RubiyaAvatar from '../../assets/images/Ellipse344.png'
+import RichardAvatar from '../../assets/images/Ellipse348.png'
+import RebecaAvatar from '../../assets/images/Ellipse346.png'
+import SadieAvatar from '../../assets/images/Ellipse347.png'
+import EricAvatar from '../../assets/images/Ellipse350.png'
+
 import LeftArrow from '../../assets/images/arrow-left.svg'
 import RightArrow from '../../assets/images/arrow-right.svg'
 
@@ -10,15 +15,19 @@ export const Testimonials = () => {
   return (
     <Card>
       <div>
-        <AvatarImg src={RubiyaAvatar} alt='' />
+        <AvatarImg src={RubiyaAvatar} alt='RubiyaAvatar' />
       </div>
       <p>Shokworks are dedicated and they accomplished all my ideas and thoughts for my project, excellent guidance.</p>
       <h4>Rubiya Mond De-Patrica</h4>
       <span>CEO, Zexbay-fine</span>
       <ArrowContainer>
-        <img src={LeftArrow} alt='' />
-        <img src={RightArrow} alt='' />
+        <img src={LeftArrow} alt='LeftArrow' />
+        <img src={RightArrow} alt='RightArrow' />
       </ArrowContainer>
+      <RichardImg src={RichardAvatar} alt='RichardAvatar' />
+      <RebecaImg src={RebecaAvatar} alt='RebecaAvatar' />
+      <SadieImg src={SadieAvatar} alt='SadieAvatar' />
+      <EricImg src={EricAvatar} alt='EricAvatar' />
     </Card>
   )
 }
--- src/Components/Testimonials/styles.js
+++ src/Components/Testimonials/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { mediaQueries } from '../../styles/MediaQueries/MediaQueries'
+
 import * as fonttheme from '../../styles/variables/FontThemes'
 
 import QuoteLeft from '../../assets/images/quote-left.svg'
@@ -11,9 +13,10 @@ export const Card = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
+  position: relative;
+  margin-top: 50px;
   padding: 20px;
   font-family: ${fonttheme.font2};
-
 
   p {
     color: #333333;
@@ -27,10 +30,26 @@ export const Card = styled.div`
     background-size: 25px;
     background-position: top left, top right;
     background-repeat: no-repeat;
+
+    ${mediaQueries('laptop')`
+      width: 650px;
+      padding: 0 40px;
+      font-size: 24px;
+      background-size: 50px;
+    `}
   }
 
   span {
     font-size: 12px;
+    ${mediaQueries('laptop')`
+      font-size: 17px;
+    `}
+  }
+
+  h4 {
+    ${mediaQueries('laptop')`
+      font-size: 19px;
+    `}
   }
 `
 
@@ -44,9 +63,76 @@ export const ArrowContainer = styled.div`
   gap: 20px;
   margin-top: 18px;
 
+  ${mediaQueries('laptop')`
+      gap: 40px;
+    `}
+
   img {
     width: 10px;
     height: 10px;
     cursor: pointer;
+
+    ${mediaQueries('laptop')`
+      width: 18px;
+      height: 18px;
+    `}
   }
+`
+
+export const RichardImg = styled.img`
+  display: none;
+
+  ${mediaQueries('laptop')`
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 160px;
+    width: 80px;
+  `}
+
+${mediaQueries('laptopxl')`
+    right: 360px;
+    width: 110px;
+  `}
+`
+
+export const RebecaImg = styled(RichardImg)`
+
+  ${mediaQueries('laptop')`
+    top: 150px;
+    right: 1200px;
+    width: 60px;
+  `}
+
+  ${mediaQueries('laptopxl')`
+      right: 1600px;
+      width: 80px;
+    `}
+`
+
+export const SadieImg = styled(RichardImg)`
+
+  ${mediaQueries('laptop')`
+    top: 350px;
+    right: 1120px;
+    width: 70px;
+  `}
+
+  ${mediaQueries('laptopxl')`
+    right: 1480px;
+    width: 110px;
+  `}
+`
+
+export const EricImg = styled(RichardImg)`
+
+  ${mediaQueries('laptop')`
+    top: 180px;
+    right: 50px;
+    width: 70px;
+  `}
+
+  ${mediaQueries('laptopxl')`
+    right: 185px;
+  `}
 `
